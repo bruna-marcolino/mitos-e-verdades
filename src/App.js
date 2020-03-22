184dbbc56580a63e82ad45bf4b95e1fcc79c56ea
Jefferson Daniel <jeffersondanielss@gmail.com>
2020-03-22 18:42:57 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.jpg';
+import './App.scss';
+
+function Question() {
+  return (
+    <h3 className="title">Tomar agua depois de comer doce da diabetes!</h3>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="wrapper">
+      <header className="header">
+        <img src={logo} className="header__logo" alt="logo" />
       </header>
-    </div>
+      <h2 class="questions-number">
+        Questão 1 de 10
+      </h2>
+      {Question()}
+      <section className="action-buttons">
+        <button className="btn btn--secondary">Mito</button>
+        <button className="btn btn--primary">Verdade</button>
+      </section>
+    </main>
   );
 }
 
