7e944e63f5f483063ecdfe86e5e284f34d427141
Jefferson Daniel <jeffersondanielss@gmail.com>
2020-03-22 21:25:56 -0300

    Updates

---
+++ precache-manifest.fbb64b5d4db6618d004f5d4b47543fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d15c06956cbcb44276b92e26c2ae36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a59537d0d1747e21c4",
+    "url": "/static/css/main.05b4132c.chunk.css"
+  },
+  {
+    "revision": "3f10f28afdfb1b81d18d",
+    "url": "/static/js/2.35f026b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35f026b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a59537d0d1747e21c4",
+    "url": "/static/js/main.8660be3a.chunk.js"
+  },
+  {
+    "revision": "3bb02338304888f7e826",
+    "url": "/static/js/runtime-main.cee7b8e8.js"
+  }
+]);
